74d12003297199f43a0a5ad2b9ba1c1c96ea572f
Emerson <emersonozoria@gmail.com>
2020-02-07 10:57:00 -0400

    se modifico el archivo app

--- App.js
+++ App.js
@@ -9,8 +9,19 @@ class Product {
 }
 
 class UI {
-    addProduct() {
-        
+    addProduct(Product) {
+        const productList = document.getElementById('product-list');
+        const element = document.createElement('div');
+        element.innerHTML = `
+            <div class="card text-center mb-4">
+                <div class="card-body">
+                    <strong>Product Name</strong>: ${Product.name}
+                    <strong>Product Price</strong>: ${Product.price}
+                    <strong>Product Year</strong>: ${Product.year}
+                </div>
+            </div>
+        `;
+        productList.appendChild(element);
     }
     
     deleteProduct() {
@@ -24,6 +35,16 @@ class UI {
 
 // DOM Events
 
-document.getElementById('product-form').addEventListener('submit', function (){
-                                                         alert('Enviando formulario')
-                                                         })
+document.getElementById('product-form')
+    .addEventListener('submit', function (e){
+    const name = document.getElementById('name').value;
+    const price = document.getElementById('price').value;
+    const year = document.getElementById('year').value;
+    
+    const product = new Product(name, price, year);
+    
+    const ui = new UI();
+    ui.addProduct(product);
+    
+    e.preventDefault();
+});
