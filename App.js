9c5b6fe4ddd5987130a1c9cd2e6124df1558a0d4
Emerson <emersonozoria@gmail.com>
2020-02-07 10:00:23 -0400

    se esta estructurando el archivo

--- App.js
+++ App.js
@@ -1,1 +1,29 @@
-alert('hello')
+class Product {
+    constructor(name, price, year) {
+        this.name = name;
+        this.price = price;
+        this.year = year;
+        
+    }
+    
+}
+
+class UI {
+    addProduct() {
+        
+    }
+    
+    deleteProduct() {
+        
+    }
+    
+    showMessage() {
+        
+    }
+}
+
+// DOM Events
+
+document.getElementById('product-form').addEventListener('submit', function{
+                                                         alert('Enviando formulario');
+                                                         })
